49ff08deb2e86e51a53880a74b7c8d1aa0345acd
fsiguret <fl.si@orange.fr>
2020-10-06 16:30:50 +0200

    delete ratelimit on commentary

--- backend/middleware/rateLimitCommentary.js
+++
@@ -1,7 +0,0 @@
-const rateLimitCommentary = require('express-rate-limit');
-
-module.exports = rateLimitCommentary({
-    windowMs: 0.5 * 60 * 1000, //30 seconds
-    max: 1,
-    message: "Vous ne pouvez commenter qu'une fois toute les 30 secondes."
-});
--- backend/routes/articles.js
+++ backend/routes/articles.js
@@ -3,14 +3,13 @@ const router = express.Router();
 const articlesCtrl = require('../controllers/articles');
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-configuration');
-const rateLimiteCommentary = require('../middleware/rateLimitCommentary');
 
 router.get('/', auth, articlesCtrl.getAllArticles);
 router.get('/:id', auth, articlesCtrl.getOneArticle);
 router.get('/:id/comment', auth, articlesCtrl.getCommentByArticle);
 router.get('/comment/:id', auth, articlesCtrl.getOneComment);
 router.post('/', multer, auth, articlesCtrl.addArticle);
-router.post('/:id/comment',auth, rateLimiteCommentary, articlesCtrl.commentArticle);
+router.post('/:id/comment',auth, articlesCtrl.commentArticle);
 router.put('/:id/like', auth, articlesCtrl.likeOrDislike);
 router.put('/:id',  multer, auth, articlesCtrl.changeArticle);
 router.put('/comment/:id', auth, articlesCtrl.editCommentary);
